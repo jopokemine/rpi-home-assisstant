e38635a2d874155181aef53e92d6a507c121e47f
jopokemine <josh4050@googlemail.com>
2022-08-16 14:08:38 +0100

    added display, life360, tuya, and started setup script

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 CONFIG = {
-    "MEROSS_EMAIL": os.environ.get('MEROSS_EMAIL'),
-    "MEROSS_PASSWORD": os.environ.get('MEROSS_PASSWORD'),
+    "MEROSS_EMAIL": "",
+    "MEROSS_PASSWORD": "",
     "MAC_ADDR_FILE": "mac_addrs.csv",
     "INTERFACE": "wlan0"
 }
--- main.py
+++ main.py
@@ -2,23 +2,32 @@ import asyncio
 from scanner import get_users_connection_status
 from smart_devices import turn_off_lights
 from config import CONFIG
+import os
+from datetime import datetime
 
 if __name__ == "__main__":
     users_conn_status = get_users_connection_status(CONFIG["MAC_ADDR_FILE"], CONFIG["INTERFACE"])
     users_disconnected = []
-    for user in users_conn_status:
-        if user[1]:
-            print(f"{user[0]} is connected to the {user[2]} network.")
-        else:
-            print(f"{user[0]} is disconnected.")
-            users_disconnected.append(user[0])
+    now = datetime.now()
+    curr_time = now.strftime("%H:%M")
+    with open("users_conn_status.txt", 'w') as f:
+        for user in users_conn_status:
+            if user[1]:
+                print(f"{user[0]} is connected to the {user[2]} network at {curr_time}")
+                f.write(f"{user[0]} is connected to the {user[2]} network at {curr_time}\n")
+            else:
+                print(f"{user[0]} is disconnected at {curr_time}")
+                f.write(f"{user[0]} is disconnected at {curr_time}\n")
+                users_disconnected.append(user[0])
 
     # if [user[0] for user in users_conn_status] == users_disconnected:
     #     # All devices disconnected.
     #     turn_off_lights()
     # else:
     #     for user in users_disconnected:
-    #         turn_off_lights(user)
+    #         if user != "mum":
+    #             # TODO: Finish this - tuya?
+    #             turn_off_lights(user)
 
         # if not user[1] and user[0] != "mum":
         #     print(f"{user[0]} is not connected, turning off devices...")
